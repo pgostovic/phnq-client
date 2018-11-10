d9291fa0120f106072d4c4e4abd08ab544b4ebe0
Patrick Gostovic <pg@phranq.com>
2018-11-10 10:24:06 -0500

    Expose configure

--- index.client.js
+++ index.client.js
@@ -1,8 +1,9 @@
-import { api, onApiError as _onApiError } from './src';
+import { api, configure as _configure, onApiError as _onApiError } from './src';
 import model from './src/model';
 
 export const { Artist, Tag, Taggable, Track, Album, PlayerState, Event } = model;
 
+export const configure = _configure;
 export const onApiError = _onApiError;
 
 export default api;
