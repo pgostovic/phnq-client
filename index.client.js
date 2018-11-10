3601af07ed9f5e53dc42ac42dc074c25eb7de28b
Patrick Gostovic <pg@phranq.com>
2018-11-10 11:25:22 -0500

    Test

--- index.client.js
+++ index.client.js
@@ -1,7 +1,7 @@
 import { api, configure as _configure, onApiError as _onApiError } from './src';
-// import model from './src/model';
+import model from './src/model';
 
-// export const { Artist, Tag, Taggable, Track, Album, PlayerState, Event } = model;
+export const { Artist, Tag, Taggable, Track, Album, PlayerState, Event } = model;
 
 export const configure = _configure;
 export const onApiError = _onApiError;
